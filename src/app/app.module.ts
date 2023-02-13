888c8cb3e4d32d7c53179382667fdc32c1cc5453
Karla Davalos <karlad0791@gmail.com>
2023-02-13 00:13:31 -0600

    Firebase Installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -28,7 +29,8 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  formGroup!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [ Validators.required, Validators.email ]],
+      password: ['', Validators.required]
+    });
+  }
+
+  createUser() {
+    console.log('Form Group', this.formGroup);
+    console.log('Valid?', this.formGroup.valid);
+    console.log('Value', this.formGroup.value);
+  }
 }
